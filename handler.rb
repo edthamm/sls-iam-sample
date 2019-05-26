c8aa7e462162a6e62f184651a75038b8392862a2
Eduard Thamm <eduard.thamm@thammit.at>
2019-05-26 11:30:18 +0200

    Improve logging

--- handler.rb
+++ handler.rb
@@ -1,11 +1,11 @@
 require 'aws-sdk-s3'
 
-s3 = Aws::S3::Resource.new
-bucket = s3.bucket(ENV['BUCKET_PREFIX']+'-'+ENV['STAGE']+'-sls-testing-bucket')
 
 def hello(event:, context:)
-  puts "Event: #{event}"
-  puts "Context: #{context}"
-  object = bucket.put_object(body: Time.now.utc, key: Time.now)
+  s3 = Aws::S3::Resource.new
+  bucket = s3.bucket(ENV['BUCKET_PREFIX']+'-'+ENV['STAGE']+'-sls-testing-bucket')
+  puts "Event: #{event.inspect}"
+  puts "Context: #{context.inspect}"
+  object = bucket.put_object(body: Time.now.utc.to_s, key: Time.now.to_s)
   object
 end
