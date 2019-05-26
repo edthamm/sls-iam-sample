1ed604375997b74c77b33cc1ae5c0f9c85dd1be6
Eduard Thamm <eduard.thamm@thammit.at>
2019-05-26 11:52:25 +0200

    Move init out of function

--- handler.rb
+++ handler.rb
@@ -1,11 +1,10 @@
 require 'aws-sdk-s3'
-
+S3 = Aws::S3::Resource.new
+BUCKET = S3.bucket(ENV['BUCKET_PREFIX']+'-'+ENV['STAGE']+'-sls-testing-bucket')
 
 def hello(event:, context:)
-  s3 = Aws::S3::Resource.new
-  bucket = s3.bucket(ENV['BUCKET_PREFIX']+'-'+ENV['STAGE']+'-sls-testing-bucket')
   puts "Event: #{event.inspect}"
   puts "Context: #{context.inspect}"
-  object = bucket.put_object(body: Time.now.utc.to_s, key: Time.now.to_s)
+  object = BUCKET.put_object(body: Time.now.utc.to_s, key: Time.now.to_s)
   object
 end
